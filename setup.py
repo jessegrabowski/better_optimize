301022b2177e3771e5d349ab563d462e96f78064
Jesse Grabowski <jessegrabowski@gmail.com>
2024-08-17 17:52:36 +0800

    Determine version in setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
 from setuptools import setup
+from setuptools.dist import Distribution
 
-setup()
+import versioneer
+
+dist = Distribution()
+dist.parse_config_files()
+
+
+NAME: str = dist.get_name()  # type: ignore
+
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+    )
